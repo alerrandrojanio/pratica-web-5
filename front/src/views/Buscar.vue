e5275bed5e3ab55177d0f65bf8ab9adf42a95495
lucas <lucaslmota0431@gmail.com>
2021-08-11 20:45:14 -0300

    Todo o front

---
+++ front/src/views/Buscar.vue
@@ -0,0 +1,147 @@
+<template>
+ <div class="campos">
+
+          <h4>Mostrar todos os Funcionários</h4>
+            <button @click="TodosFunc()">Todos os Funcionários</button><br /><br />
+                <ul>
+                    <li v-for="funcionario in todosFuncionarios " :key="funcionario.id">
+                        <div class="infor">
+                            <p> Id: {{ funcionario.id }}</p>
+                            <p>Nome: {{ funcionario.nome }}</p>
+                            <p>Cargo: {{ funcionario.cargo }}</p>
+                            <p>Data de Nascimento: {{ funcionario.dataNascimento }}</p>
+                            <p>Data de entrada na empresa: {{ funcionario.dataEtrEmpresa }}</p>
+                        </div>
+                    </li>
+                </ul>
+
+        <h4>Filtrar Funcionários por Id</h4>
+        <p>Id: <input type="text" v-model="id"></p>
+          <button @click="BuscarFuncId()">Funcionários por Id</button><br /><br />
+
+          <ul>
+            <li>
+                <div class="infor">
+                    <p> Id: {{ funcionarioId.id }}</p>
+                    <p>Nome: {{ funcionarioId.nome }}</p>
+                    <p>Cargo: {{ funcionarioId.cargo }}</p>
+                    <p>Data de Nascimento: {{ funcionarioId.dataNascimento }}</p>
+                    <p>Data de entrada na empresa: {{ funcionarioId.dataEtrEmpresa }}</p>
+                </div>
+            </li>
+        </ul>
+
+        <h4>Filtrar pelo cargo do Funcionário</h4>
+             <p> Cargo: <input type="text" v-model="marca" /><br /></p>
+             <button @click="BuscarFuncCargo()">Buscar funcionário pelo cargo</button><br /><br />
+
+                <ul>  
+                    <li v-for="funcionario in funCargo " :key="funcionario.id">
+                        <div class="infor">
+                            <p> Id: {{ funcionario.id }}</p>
+                            <p>Nome: {{ funcionario.nome }}</p>
+                            <p>Cargo: {{ funcionario.cargo }}</p>
+                            <p>Data de Nascimento: {{ funcionario.dataNascimento }}</p>
+                            <p>Data de entrada na empresa {{ funcionario.dataEtrEmpresa }}</p>
+                        </div>
+                    </li>
+                </ul>
+        
+        <h4>Filtrar por quantidade de Funcionários</h4>
+            Quantidade: <input type="text" v-model="qtd" /><br /><br /><br>
+             <button @click="BuscarFuncQuant()">Buscar por qtd de funcionários</button><br /><br />
+
+              <ul>
+                     <li v-for="funcionario in funcQtd " :key="funcionario.id">
+                        <div class="infor">
+                            <p> Id: {{ funcionario.id }}</p>
+                            <p>Nome: {{ funcionario.nome }}</p>
+                            <p>Cargo: {{ funcionario.cargo }}</p>
+                            <p>Data de Nascimento: {{ funcionario.dataNascimento }}</p>
+                            <p>Data de entrada na empresa {{ funcionario.dataEtrEmpresa }}</p>
+                        </div>
+                    </li>
+               </ul>
+
+        </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            id: 0,
+            nome: "",
+            cargo: "",
+            dataNascimento:"",
+            dataEtrEmpresa:"",
+            qtd:0,
+            todosFuncionarios:[],
+            funcionarioId:[],
+            funCargo:[],
+            funcQtd:[],
+            baseURI:""
+        }
+    },
+
+    methods: {
+        TodosFunc(){
+            axios.get(this.baseURI).then((result) =>{
+                this.todosFuncionarios = result.data
+            })
+        },
+
+        BuscarFuncId(){
+            axios.get(this.baseURI + "/" + this.id).then((result) =>{
+                this.funcionarioId = result.data
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        },
+
+        BuscarFuncCargo(){
+            axios.get(this.baseURI + "/search?cargo=" + this.marca).then((result) =>{
+                this.funCargo = result.data
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        },
+
+        BuscarFuncQuant(){
+            axios.get(this.baseURI + "/procu?qtd=" + this.qtd).then((result) =>{
+                this.funcQtd = result.data
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        }
+    },
+}
+</script>
+
+<style>
+.Campos {
+    margin: auto;
+    width: 80%;
+        
+}
+button{
+    background-color: rgb(23, 91, 192);
+    color: white;
+    margin: 0 20px;
+    height: 30px;
+    cursor: pointer;
+}
+.infor {
+    display: flex;
+   
+}
+.infor p {
+    margin: 10px 10px;
+    
+    
+}
+</style>
---
+++ front/src/views/Funcionarios.vue
@@ -0,0 +1,118 @@
+<template>
+<div>
+
+
+  <div class="campos">
+    <h4>Cadastrar Carros</h4>
+
+           <p> Id: <input type="text" v-model="id" /><br /></p> 
+           <p> Nome: <input type="text" v-model="nome" /><br /></p> 
+           <p> Marca: <input type="text" v-model="cargo" /><br /></p>
+           <p> Ano de Fabricação: <input type="text" v-model="dataNascimento" /><br /></p>
+           <p> Ano do modelo: <input type="text" v-model="dataEtrEmpresa" /><br /><br></p>
+   </div>
+
+  <div class="butaos">
+    <button @click="PostFunc()">Post Funcionário</button><br /><br />
+    <button @click="PutFunc()">Put Funcionário</button><br /><br />
+    <button @click="DeleteFunc()">Delete Funcionário</button><br /><br /> 
+  </div>
+
+   <!-- <ul>
+      <li>
+        <div class="infor">
+          <p> Id: {{ carros.id }}</p>
+          <p>Nome: {{ carros.nome }}</p>
+          <p>Marca: {{ carros.marca }}</p>
+          <p>Ano de Fabricação: {{ carros.anoFabricacao }}</p>
+          <p>Ano de Modelo: {{ carros.anoModelo }}</p>
+          <p>Data de Venda: {{ carros.dataVenda }}</p>
+        </div>
+      </li>
+    </ul> -->
+
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+        id: 0,
+        nome: "",
+        cargo:"",
+        dataNascimento:"",
+        dataEtrEmpresa:"",
+        funcionarios:[],
+        baseURI:""
+    }
+  },
+  
+  methods: {
+    PostFunc(){
+      let obj ={
+        nome: this.nome,
+        cargo: this.cargo,
+        dataNascimento: this.dataNascimento,
+        dataEtrEmpresa: this.dataEtrEmpresa,
+      };
+
+      axios.post(this.baseURI, obj).then((resul) =>{
+        this.funcionarios = resul.data
+      })
+    },
+
+    PutFunc(){
+      let obj ={
+
+        nome: this.nome,
+        cargo: this.cargo,
+        dataNascimento: this.dataNascimento,
+        dataEtrEmpresa: this.dataEtrEmpresa,
+      };
+
+      axios.put(this.baseURI+"/" + this.id, obj).then((result) =>{
+        console.log(result)
+      })
+    },
+
+    DeleteFunc(){
+      axios.delete(this.baseURI +"/"+this.id,).then((result) =>{})
+    }
+  },
+}
+</script>
+
+<style>
+
+.Campos {
+    margin: auto;
+    font-weight: 600;
+    width: 40%;
+        
+}
+.butaos{
+    display: flex;
+    width: 70%;
+    justify-content: center;
+    margin: 10px auto;
+  
+}
+.butaos button{
+    background-color: rgb(23, 91, 192);
+    color: white;
+    margin: 0 20px;
+    cursor: pointer;
+}
+.infor {
+    display: flex;
+}
+.infor p {
+    margin: 10px 10px;
+    
+}
+
+
+</style>
